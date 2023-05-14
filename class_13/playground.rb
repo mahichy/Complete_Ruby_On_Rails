b09feacdfd9e7bb301cedf04d09f861b9258fc5d
mahichy <mohiuddinoshim@yahoo.com>
2023-05-14 22:08:36 +0600

    Class 13 getting input from user

---
+++ class_13/playground.rb
@@ -0,0 +1,20 @@
+# first_name = "Mahi";
+# last_name = "Chowdhury";
+
+# full_name = "My first name is #{first_name} and last name is #{last_name}"
+
+# puts "first_name: #{first_name}"
+# puts "last_name: #{last_name}"
+# puts full_name
+
+puts "What is your first name?"
+	first_name = gets.chomp
+	last_name = gets.chomp
+puts "Thank you, You said your first name #{first_name} and last name #{last_name}."
+
+
+number = "Enter a number to multiply by 2"
+puts number
+input = gets.chomp
+
+puts input.to_i * 2
